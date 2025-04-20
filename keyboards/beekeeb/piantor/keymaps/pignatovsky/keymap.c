b67f518cef59b99cf3861a059e81f82d3f0dae25
Piotr Ignatowski <pignatovsky@gmail.com>
2025-04-20 14:33:20 +0200

    Experiment with layer

--- keyboards/beekeeb/piantor/keymaps/pignatovsky/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/pignatovsky/keymap.c
@@ -19,15 +19,15 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x6_3(
 KC_TRNS, KC_Q, KC_W, KC_E, KC_R, KC_T,  KC_Y, KC_U, KC_I, KC_O, KC_SCLN, KC_TRNS,
-KC_TRNS, MT(KC_LSFT, KC_A), MT(KC_LGUI, KC_S), MT(KC_LCTL, KC_D), MT(KC_LALT, KC_F), KC_G,  KC_H, MT(KC_LALT, KC_J), MT(KC_LCTL, KC_K), MT(KC_LGUI, KC_L), MT(KC_LSFT, KC_P), KC_TRNS,
+KC_TRNS, KC_A, KC_S, KC_D, KC_F, KC_G,  KC_H, KC_J, KC_K, KC_L, KC_P, KC_TRNS,
 KC_TRNS, KC_Z, KC_X, KC_C, KC_V, KC_B,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_TRNS,
-TD(TD_LGUI_LCTRL), KC_BSPC, MO(_NUMS),               KC_RALT, KC_SPC, TD(TD_LCTRL_LGUI)
+TD(TD_LGUI_LCTRL), KC_BSPC, KC_LSFT,               TD(TD_RALT_LALT), KC_SPC, TD(TD_LCTRL_LGUI)
     ),
     [_NUMS] = LAYOUT_split_3x6_3(
 KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR, KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, KC_QUOT,
 KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,  KC_6, KC_7, KC_8, KC_9, KC_0, KC_QUOT,
-KC_GRV, KC_Z, KC_X, KC_C, KC_V, KC_B,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
-KC_LGUI, KC_BSPC, KC_LSFT,             KC_RALT, KC_SPC, KC_LCTL
+KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,  KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_BSLS,
+KC_LGUI, KC_BSPC, KC_LSFT,             KC_RALT, KC_F11, KC_F12
     ),
 };
 
@@ -56,6 +56,7 @@ const uint16_t PROGMEM home[] = {KC_W, KC_E, COMBO_END};
 const uint16_t PROGMEM end[] = {KC_X, KC_C, COMBO_END};
 // const uint16_t PROGMEM space[] = {KC_J, KC_L, COMBO_END};
 const uint16_t PROGMEM bkspace[] = {KC_S, KC_F, COMBO_END};
+const uint16_t PROGMEM nums[] = {KC_BSPC, KC_SPC, COMBO_END};
 
 combo_t key_combos[] = {
         COMBO(left_paren, KC_LPRN),
@@ -83,4 +84,5 @@ combo_t key_combos[] = {
         COMBO(quote, KC_QUOT),
         COMBO(home, KC_HOME),
         COMBO(end, KC_END),
+        COMBO(nums, MO(_NUMS)),
 };
